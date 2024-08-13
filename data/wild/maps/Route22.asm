e5f2910400da25bbc4ec048ec82633bee660aed5
gas-was-taken <166496493+gas-was-taken@users.noreply.github.com>
2024-08-13 10:56:22 +0200

    Remove versions exclusives wild encounters

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,18 +1,6 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db  3, RATTATA
-IF DEF(_RED)
-	db  3, NIDORAN_M
-	db  4, RATTATA
-	db  4, NIDORAN_M
-	db  2, RATTATA
-	db  2, NIDORAN_M
-	db  3, SPEAROW
-	db  5, SPEAROW
-	db  3, NIDORAN_F
-	db  4, NIDORAN_F
-ENDC
-IF DEF(_BLUE)
 	db  3, NIDORAN_F
 	db  4, RATTATA
 	db  4, NIDORAN_F
@@ -22,7 +10,6 @@ IF DEF(_BLUE)
 	db  5, SPEAROW
 	db  3, NIDORAN_M
 	db  4, NIDORAN_M
-ENDC
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
